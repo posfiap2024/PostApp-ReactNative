889bea1b5f972ab8cb1fd0356b77d3d26737aec2
RenanPmtl <renangallucci@gmail.com>
2024-11-29 17:53:59 -0300

    Ajustes nos imports

--- contexts/AuthContext.js
+++ contexts/AuthContext.js
@@ -1,6 +1,6 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import { logarUsuario, obterUsuario } from '../services/api';
+import { logarUsuario, obterUsuario } from '../services/users';
 
 const AuthContext = createContext();
 
